6cc24a46c7d13385d41f8f595b628d8615ba3df7
ItsSavo <77900070+ItsSavo@users.noreply.github.com>
2022-12-01 00:27:57 +0000

    Player added +can move left/right + basic level

---
+++ RunSantaRun/Assets/Scripts/LevelBoundary.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundary : MonoBehaviour
+{
+    public static float leftSide = -15f;
+    public static float rightSide = -10f;
+    public float internalLeft;
+    public float internalRight;
+
+    void Update()
+    {
+        internalLeft = leftSide;
+        internalRight = rightSide;
+    }
+}
